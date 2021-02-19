868b8dc5c3f552b582780e597915b8c5f812226e
liliangabc <ll917274996@live.com>
2021-02-19 17:42:54 +0800

    完善首页界面

--- pages/about_us.vue
+++ pages/about_us.vue
@@ -18,26 +18,16 @@
           </div>
         </div>
       </div>
-      <div class="row-2">
-        <img src="/images/CTA-2-1.jpg" alt="">
-        <div class="inner">
-          <div>
-            <h3>开始您的免费咨询</h3>
-            <p>我们专业的客服人员将在24小时内给您回复。</p>
-            <nuxt-link to="/contact">
-              <b class="button">点击联系我们</b>
-            </nuxt-link>
-          </div>
-        </div>
-      </div>
+      <ui-advisory-card />
     </div>
   </ui-main>
 </template>
 
 <script>
 import UiMain from '@/components/Main.vue'
+import UiAdvisoryCard from '@/components/AdvisoryCard.vue'
 export default {
-  components: { UiMain },
+  components: { UiMain, UiAdvisoryCard },
   data () {
     return {
       title: '关于我们'
@@ -51,13 +41,6 @@ export default {
 
 <style lang="scss">
 .page-about {
-  .row-1, .row-2 {
-    img {
-      display: block;
-      object-fit: cover;
-      width: 100%;
-    }
-  }
   .row-1 {
     padding: 100px 1rem;
     .inner {
@@ -70,6 +53,9 @@ export default {
           margin-left: 9%;
         }
         img {
+          display: block;
+          object-fit: cover;
+          width: 100%;
           max-height: 550px;
         }
         h3 {
@@ -82,37 +68,6 @@ export default {
           font-size: 1.1rem;
           line-height: 1.6;
         }
-      }
-    }
-  }
-  .row-2 {
-    position: relative;
-    img {
-      min-height: 265px;
-    }
-    .inner {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      text-align: center;
-      background-color: rgba(0, 0, 0, 0.6);
-      color: #fff;
-      h3 {
-        font-size: 1.9rem;
-      }
-      p {
-        font-size: 1.25rem;
-        margin: 1em;
-      }
-      .button {
-        @include gradBtn;
-        padding: .6rem 1.7rem;
-        font-size: .9rem;
       }
     }
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,144 @@
+/* eslint-disable vue/no-v-html */
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        sindvision.com
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="page-index">
+    <div class="row-1">
+      <div class="inner">
+        <h3 class="title">
+          云客服平台
+        </h3>
+        <p class="desc">
+          云客服平台，以通信和云计算为基础，具备全渠道全触点接入、优质的网络与号码资源、大数据及智能化应用的智能联络平台。
+          致力于为企业提升客服工作效率，降低管理成本，让企业的客户服务成为新的商业价值。
+        </p>
       </div>
     </div>
+    <div class="row-2">
+      <ul class="clearfix inner">
+        <li v-for="_ in row2Data" :key="_.title">
+          <div class="box">
+            <h3 class="title">
+              {{ _.title }}
+            </h3>
+            <p class="desc" v-html="_.desc" />
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="row-3">
+      <div class="inner">
+        <img src="/images/agency-seo-desk-front.jpg" alt="">
+      </div>
+    </div>
+    <ui-advisory-card />
   </div>
 </template>
 
 <script>
-export default {}
+import UiAdvisoryCard from '@/components/AdvisoryCard.vue'
+export default {
+  components: { UiAdvisoryCard },
+  data () {
+    return {
+      row2Data: [
+        {
+          title: '需要呼叫中心的企业',
+          desc: '传统呼叫中心升级换代<br>客户联络触点增加<br>智能化应用需求'
+        },
+        {
+          title: '呼叫中心能力提供者',
+          desc: '丰富的客户联络运营经验积累<br>向移动互联网服务专线发展'
+        },
+        {
+          title: '应用开发合作方',
+          desc: '更丰富的应用组件<br>更智能的技术组合'
+        },
+        {
+          title: '基础设置资源提供者',
+          desc: '云主机+云储存+云服务<br>成为主流系统架构'
+        },
+        {
+          title: '电信运营商',
+          desc: '需要规范、优质的企业客户<br>提供更优质的基础设置（管道）'
+        },
+        {
+          title: '职场/话务员',
+          desc: '万众创业下的众包服务<br>快速承接业务的运营能力'
+        }
+      ]
+    }
+  }
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="scss">
+.page-index {
+  background-color: #f7f8f9;
+  .row-1 {
+    padding: 10rem $contentPadding 5rem;
+    .inner {
+      max-width: 600px;
+      margin: 0 auto;
+      text-align: center;
+      .title {
+        font-size: 2rem;
+        margin-bottom: 20px;
+      }
+      .desc {
+        color: #5d5f64;
+        font-size: 1.3rem;
+        line-height: 1.6;
+      }
+    }
+  }
+  .row-2 {
+    padding: 0 $contentPadding 2rem;
+    .inner {
+      max-width: $contentWidth;
+      margin: 0 auto;
+      list-style: none;
+      li {
+        display: inline-block;
+        width: 33.33%;
+        padding: 10px;
+        vertical-align: top;
+      }
+      .box {
+        padding: 2.1rem;
+        background-color: #fff;
+        text-align: center;
+        min-height: 240px;
+      }
+      .title {
+        font-size: 1.7rem;
+        margin-bottom: 1rem;
+      }
+      .desc {
+        font-size: 1.1rem;
+        color: #5d5f64;
+        line-height: 1.6;
+      }
+      @media screen and (max-width: 800px) {
+        li {
+          width: 100%;
+          padding: 5px 0;
+        }
+        .box {
+          min-height: auto;
+        }
+      }
+    }
+  }
+  .row-3 {
+    padding: 0 $contentPadding;
+    .inner {
+      max-width: $contentWidth;
+      margin: 0 auto;
+      text-align: center;
+      img {
+        display: block;
+        width: 100%;
+      }
+    }
+  }
 }
 </style>
